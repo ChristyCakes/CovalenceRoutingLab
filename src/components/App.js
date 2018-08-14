66ecbc6509ce8e0fc175be24d87eea79ec9e1de9
Christy Welsh <christyannwelsh@gmail.com>
2018-08-14 09:05:30 -0700

    restructure

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, NavLink, Switch, Route } from 'react-router-dom';
-import Films from './Films';
+import { FilmsArray } from './Films';
 import People from './People';
 import Home from './Home';
 import '../App.css';
@@ -13,12 +13,12 @@ class App extends Component {
           <div className='container'>
             <NavLink exact to="/" activeClassName="active"><button>Go Home</button></NavLink>
             <NavLink to="/films" activeClassName="active"><button>View Films</button></NavLink>
-            <NavLink to="/People" activeClassName="active"><button>View People</button></NavLink>
+            {/* <NavLink to="/People" activeClassName="active"><button>View People</button></NavLink> */}
           </div>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/films" component={Films} />
-            <Route path="/people" component={People} />
+            <Route path="/films" component={FilmsArray} />
+            {/* <Route path="/people" component={People} /> */}
           </Switch>
         </Fragment>
       </Router>
--- src/components/Films.jsx
+++ src/components/Films.jsx
@@ -4,51 +4,101 @@ import 'es6-promise';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
-import Film from './Film.jsx'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 
-class Films extends Component {
+// ---------------- 1st Component: retrieve all films -------------------------------------
+class FilmsArray extends Component {
     constructor() {
         super();
-        this.state = { cards: [] }
+        this.state = { filmsArray: [] }
     }
-
     componentDidMount() {
+        // when new page loads after clicking 'view films' button, fetch films data from API
         fetch("https://ghibliapi.herokuapp.com/films")
+            // convert data from JSON to JS object
             .then(response => response.json())
+            // take that object, which is an array of film objects, and set state with it
             .then(films => {
                 this.setState({
-                    cards: films.map(film => {
+                    // map over each film object in the array and send the info to FilmsCards component to display each film in a card
+                    filmsArray: films.map(film => {
                         return (
-                            <Link to={`/films/${film.id}`} style={{ textDecoration: "none" }} key={film.id}>
-                                <Card >
-                                    <CardContent>
-                                        <Typography variant="headline">{film.title}</Typography>
-                                        <Typography>Directed By: {film.director}</Typography>
-                                        <Typography color="textSecondary">{film.description}</Typography>
-                                    </CardContent>
-                                </Card>
-                            </Link>
+                            // set a key value with the film's pre-set id, use ...spread operator to grab all the objects for each film
+                            <FilmsCards key={film.id} {...film} />
                         );
-                    }),
+                    })
                 });
             })
-            .catch(e => console.log(e))
+            .catch(e => console.log(e));
     };
-
     render() {
-        return (
-            <div>
-                {this.state.cards}
-                <Router >
-                    <Fragment >
-                        <Route path="/films/:id" component={Film} />
-                    </Fragment>
-                </Router>
-            </div>
-        )
+        // render cards by calling state between divs
+        return (<div>{this.state.filmsArray}</div>)
     }
 }
 
-export default Films;
+
+// ------------------- 2nd Component: display array of film cards  -------------------------------
+
+// from all the objects in the film array, grab only 4 key/value properties 
+const FilmsCards = ({ id, title, director, description }) => {
+    return (
+        // set up routes
+        <Router>
+            <Fragment>
+                {/* make each card a link, using the film id as the endpoint */}
+                <Link to={`/films/${id}`} style={{ textDecoration: "none" }}>
+                    <Card >
+                        <CardContent>
+                            <Typography variant="headline">{title}</Typography>
+                            <Typography>Directed By: {director}</Typography>
+                            <Typography color="textSecondary">{description}</Typography>
+                        </CardContent>
+                    </Card>
+                </Link>
+                {/* route to Film component when a film card is clicked */}
+                <Route path="/films/:id" component={Film} />
+            </Fragment>
+        </Router>
+    )
+}
+
+
+// -------------------------  3rd Component retrieve, display one film when card is clicked -----------------
+class Film extends Component {
+    constructor() {
+        super();
+        this.state = { film: {} }
+    }
+    componentDidMount() {
+        // retrieve API data on the clicked film (match is passed in the route)
+        fetch(`https://ghibliapi.herokuapp.com/films/${this.props.match.params.id}`)
+            .then(res => res.json())
+            .then(film => {
+                // store the individual film object details in state
+                this.setState({ film: { ...film } })
+            })
+            .catch(e => console.log(e));
+    }
+
+    render() {
+        let filmDeets= [];
+        if (this.state) {
+            for (let key in this.state.film) {
+                if (this.state.film.hasOwnProperty(key)) {
+                    let detail = key + ": " + this.state.film[key]
+                   filmDeets.push(detail)
+                    console.log(key + ":" + this.state.film[key])
+                }
+            }
+        }
+        return <div>{filmDeets}</div>
+    }
+}
+
+export { FilmsArray, FilmsCards, Film };
+
+// Questions:
+// how can I shorten this.props.match.params.id?
+// should I create a 4th component to display the one clicked film?
